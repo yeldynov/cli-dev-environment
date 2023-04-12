1cc3951e3845a1b335f71ca03fc669947115cfb7
Nico Yeldynov <yeldynov@gmail.com>
2023-04-12 14:07:12 +0300

    binded input from component to the bundling plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localforage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -39,9 +39,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, {useState} from 'react'
-            `,
+            contents: inputCode,
           };
         }
 
